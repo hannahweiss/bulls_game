9225f4108ac0733f897a2092cf50753da45d2e45
homework 5 <hw5@guest.guest>
2021-02-18 16:35:04 +0000

    deploy and start scripts

--- start.sh
+++ start.sh
@@ -11,15 +11,15 @@
 
 export SECRET_KEY_BASE=W68eso5YQOlbtvSNUR50N/HDWj6IaEhAwMR3LtzuBEQAefwYVbX84bvoTA7XtiGi
 export MIX_ENV=prod
-export PORT=4770
+export PORT=4790
 
 echo "Stopping old copy of app, if any..."
 
-_build/prod/rel/practice/bin/practice stop || true
+_build/prod/rel/bulls/bin/bulls stop || true
 
 echo "Starting app..."
 
-_build/prod/rel/practice/bin/practice start
+_build/prod/rel/bulls/bin/bulls start
 
 # TODO: Add a systemd service file
 #       to start your app on system boot.
